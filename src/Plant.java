4a17ca33be3679316b1f28a700941b2735d44e40
aupradies <gdwotek@gmail.com>
2024-01-13 11:02:13 +0100

    stuff

--- src/Plant.java
+++ src/Plant.java
@@ -1,6 +1,9 @@
-public enum Plant {
-    Shooter,iceShooter,doubleShooter,
-    Flower,Walnut,Mine
+public class Plant {
+    enum type{
+        Shooter,iceShooter,doubleShooter,
+        Flower,Walnut,Mine
+    }
+    int x;
+    int y;
+    int hp = 100;
 }
-
-

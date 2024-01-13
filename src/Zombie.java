a6b83e83f05369b318075c3dbb9db02395f4372a
supergarrygamer <supergarrygamer@gmail.com>
2024-01-13 10:59:39 +0100

    zombieing

--- src/Zombie.java
+++ src/Zombie.java
@@ -1,2 +1,25 @@
-public class Zombie {
+public class Zombie extends GameObject {
+    public Zombie(double x, double y) {
+        super(x, y);
+    }
+
+    public void assignToTrack(int track) {
+        switch (track) {
+            case 0:
+                this.x = 0;
+                break;
+            case 1:
+                this.x = 0;
+                break;
+            case 2:
+                this.x = 0;
+                break;
+            case 3:
+                this.x = 0;
+                break;
+            case 4:
+                this.x = 0;
+                break;
+        }
+    }
 }

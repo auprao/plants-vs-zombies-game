6b4be7f8e246d7925cd0622043f85da121d1f9a9
supergarrygamer <supergarrygamer@gmail.com>
2024-01-13 12:14:50 +0100

    zombiewaas walkeing

--- src/Zombie.java
+++ src/Zombie.java
@@ -3,13 +3,30 @@ import javafx.scene.image.ImageView;
 
 public class Zombie extends GameObject {
 
-    private enum Anim_State {
+    static final Image[] WALK_FRAMES = {
+            new Image("file:Archive/walk/go_1.png"),
+            new Image("file:Archive/walk/go_2.png"),
+            new Image("file:Archive/walk/go_3.png"),
+            new Image("file:Archive/walk/go_4.png"),
+            new Image("file:Archive/walk/go_5.png"),
+            new Image("file:Archive/walk/go_6.png"),
+            new Image("file:Archive/walk/go_7.png"),
+            new Image("file:Archive/walk/go_8.png"),
+            new Image("file:Archive/walk/go_9.png"),
+            new Image("file:Archive/walk/go_10.png")
+    };
+    private int track = 0;
+
+    private enum AnimStates {
         walk, attack, die
     }
+
+    private AnimStates animState = AnimStates.walk;
     static double walkSpeed = 1;
     public ImageView img = new ImageView(new Image("file:Archive/zombie.png"));
     public int hp = 100;
-    public int track = 0;
+    public int frame = 0;
+
     public Zombie(double x, double y) {
         super(x, y);
         img.setScaleX(0.4);
@@ -38,6 +55,13 @@ public class Zombie extends GameObject {
     }
 
     public void update() {
+        if (Math.random() < 0.4) {frame++;}
+
+        if (this.animState == AnimStates.walk) {
+            if (frame >= WALK_FRAMES.length) frame = 0;
+            this.img.setImage(WALK_FRAMES[frame]);
+        }
+
         if (this.hp <= 0) {
             die();
         }

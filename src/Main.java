3a286569218e0caf23021a68cd1022473892e523
aupradies <gdwotek@gmail.com>
2024-01-13 10:16:37 +0100

    kaka

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.time.chrono.JapaneseChronology;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        JapaneseChronology japaneseChronology = 8;
     }
 }

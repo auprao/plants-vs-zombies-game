5d94ef6aefd4cf561673d9d5dd501a9b783ba59d
JeiCamMetsej <mzdanowiczm@gmail.com>
2024-01-13 10:18:20 +0100

    condemn him to the infirmary

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Main extends Application {
+    static final int FPS = 60;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(-1);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis((double) 1000/FPS)));
     }
 }

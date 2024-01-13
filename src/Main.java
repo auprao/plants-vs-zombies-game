8f9c40b245cc8990fe43f8cc0938d03fb9246154
JeiCamMetsej <mzdanowiczm@gmail.com>
2024-01-13 10:27:09 +0100

    base app done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
     static final int FPS = 60;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         Timeline timeline = new Timeline();
         timeline.setCycleCount(-1);
-        timeline.getKeyFrames().add(new KeyFrame(Duration.millis((double) 1000/FPS)));
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis((double) 1000/FPS), e -> {
+            // Run every frame
+        }));
+        timeline.play();
+
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }

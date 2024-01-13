72ed8eec1492a2ec40b2cf5d0c3aba867e69deb8
furretcat <przygoda.alex@gmail.com>
2024-01-13 11:40:54 +0100

    buttons to spawn zombies

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.media.Media;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -32,12 +33,36 @@ public class Main extends Application {
         Media bgMusic = new Media(new File(path).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(bgMusic);
         mediaPlayer.setAutoPlay(true);
-
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-
-
         background.setImage(bg);
         root.getChildren().add(background);
+
+        Button row1Button = new Button("\u200E \u200E \u200E \u200E ");
+        Button row2Button = new Button("\u200E \u200E \u200E \u200E ");
+        Button row3Button = new Button("\u200E \u200E \u200E \u200E ");
+        Button row4Button = new Button("\u200E \u200E \u200E \u200E ");
+        Button row5Button = new Button("\u200E \u200E \u200E \u200E ");
+
+        double X = bg.getWidth()-bg.getWidth()/10;
+        row1Button.setLayoutX(X);
+        row2Button.setLayoutX(X);
+        row3Button.setLayoutX(X);
+        row4Button.setLayoutX(X);
+        row5Button.setLayoutX(X);
+
+        row1Button.setLayoutY(120);
+        row2Button.setLayoutY(220);
+        row3Button.setLayoutY(330);
+        row4Button.setLayoutY(440);
+        row5Button.setLayoutY(550);
+
+
+
+        root.getChildren().addAll(row1Button, row2Button, row3Button,row4Button, row5Button);
+
+
+
+
 
         Timeline timeline = new Timeline();
         timeline.setCycleCount(-1);
